57bce76aa71bc980cbfaa2849ae51fd688382ff4
chengpengxing <chengpengxing@jd.com>
2022-03-16 20:31:07 +0800

    Init the project

---
+++ src/main/java/com/company/mywebsite/Main.java
@@ -0,0 +1,10 @@
+package com.company.mywebsite;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        System.out.println("aaa");
+    }
+}
