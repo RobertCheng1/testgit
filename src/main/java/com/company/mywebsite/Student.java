a548bb627ebca48b35ffb18d39539be862e90403
chengpengxing <chengpengxing@jd.com>
2022-03-16 20:36:22 +0800

    Add the student class in the dev branch

---
+++ src/main/java/com/company/mywebsite/Student.java
@@ -0,0 +1,15 @@
+package com.company.mywebsite;
+
+import lombok.Data;
+
+/**
+ * @Author: chengpengxing
+ * @Description:
+ * @File: Student
+ * @Date: 2022/3/16 20:35
+ */
+@Data
+public class Student extends Person {
+
+    private Integer score;
+}
